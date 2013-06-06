9876d98009f5c2c644f341568630915dc05cb52d
Zamechek, Shawn <zamechek@wharton.upenn.edu>
2013-06-06 18:53:56 -0400

    rotate and reflect

--- src/catsdogs/g1/Board.java
+++ src/catsdogs/g1/Board.java
@@ -1,5 +1,7 @@
 package catsdogs.g1;
 
+import javax.swing.border.Border;
+
 public class Board {
     private int[][] board;
     public Board(int[][] board){
@@ -19,5 +21,30 @@ public class Board {
     
     public boolean equals(Object obj) {
 		return false;
+    }
+    
+    private int[][] rotateCW(int[][] board) {
+        int[][] rotatedArray = new int[board.length][board[0].length];
+        for (int i = 0; i < board[0].length; i++) {
+            for (int j = board.length - 1; j >= 0; j--) {
+                rotatedArray[i][j] = board[j][i];
+            }
+        }
+        return rotatedArray;
+    }
+    
+    private int[][] reflect(int[][] board, int middle) {
+        int[][] reflectedArray = new int[board.length][board[0].length];
+        for (int i = 0; i < board[0].length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (i < middle) {
+                    reflectedArray[i][j] = board[board.length - i][j];
+                } else {
+                    reflectedArray[i][j] = board[board.length + i][j];
+                }
+                reflectedArray[i][j] = board[j][i];
+            }
+        }
+        return reflectedArray;
     }
 }
