9ee2cf85b2a72f83df06019d3b0c5f5a985a8bd2
Zamechek, Shawn <zamechek@wharton.upenn.edu>
2013-06-10 21:43:09 -0400

    CatCanMakeLosingMoveHeuristic is done and not tested

--- src/catsdogs/g1/heuristic/CatCanMakeLosingMoveHeuristic.java
+++ src/catsdogs/g1/heuristic/CatCanMakeLosingMoveHeuristic.java
@@ -1,11 +1,60 @@
 package catsdogs.g1.heuristic;
 
-public class CatCanMakeLosingMoveHeuristic extends Heuristic {
+import org.apache.log4j.Logger;
 
-	@Override
-	public double evaluate(int[][] board, int playerMove) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+import catsdogs.sim.Board;
+import catsdogs.sim.Dog;
+
+public class CatCanMakeLosingMoveHeuristic extends Heuristic {
+    private Logger logger = Logger.getLogger(this.getClass()); // for logging
+    @Override
+    public double evaluate(int[][] board, int playerMove) {
+        double possibleLosingMoves = 0;
+        for (int x = 0; x < Board.X; x++) {
+            for (int y = 0; y < Board.Y; y++) {
+                if (board[x][y] == Board.CAT && y > 0 && board[x][y-1] == Board.EMPTY) {
+                    int [][] moveLeftBoard = board;
+                    moveLeftBoard[x][y] = Board.EMPTY;
+                    moveLeftBoard[x][y-1] = Board.CAT;
+                    if (Dog.wins(moveLeftBoard)){
+                        possibleLosingMoves++;
+                    }
+                }
+                if (board[x][y] == Board.CAT && y < 6 && board[x][y-1] == Board.EMPTY) {
+                    int [][] moveUpBoard = board;
+                    moveUpBoard[x][y] = Board.EMPTY;
+                    moveUpBoard[x][y+1] = Board.CAT;
+                    if (Dog.wins(moveUpBoard)){
+                        possibleLosingMoves++;
+                    }
+                }
+                if (board[x][y] == Board.CAT && x < 6 && board[x+1][y] == Board.EMPTY) {
+                    int [][] moveRightBoard = board;
+                    moveRightBoard[x][y] = Board.EMPTY;
+                    moveRightBoard[x+1][y] = Board.CAT;
+                    if (Dog.wins(moveRightBoard)){
+                        possibleLosingMoves++;
+                    }
+                }
+                if (board[x][y] == Board.CAT && x > 0 && board[x-1][y] ==  Board.EMPTY) {
+                    int [][] moveLeftBoard = board;
+                    moveLeftBoard[x][y] = Board.EMPTY;
+                    moveLeftBoard[x][y+1] = Board.CAT;
+                    if (Dog.wins(moveLeftBoard)){
+                        possibleLosingMoves++;
+                    }
+                }
+            }
+        }
+        if (playerMove == CAT) {
+            double hValueCat = (double)possibleLosingMoves;
+            logger.info("There are " + hValueCat + " moves that result in the dog winning");
+            return hValueCat;
+        } else {
+            double hValueDog = -(double)possibleLosingMoves;
+            logger.info("There are " + Math.abs(hValueDog) + " moves that result in the dog winning");
+            return hValueDog;
+        }
+    }
 
 }
